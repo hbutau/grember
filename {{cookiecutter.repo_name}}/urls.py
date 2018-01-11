a2a0538ad6fada93565f5060b5f4359a6618aa26
hamub <hbutau35@gmail.com>
2018-01-11 23:25:51 +0200

    added docker support

--- {{cookiecutter.repo_name}}/urls.py
+++ {{cookiecutter.repo_name}}/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 
 from graphene_django.views import GraphQLView 
 
-from {{cookiecutter.repo_name}}.schema import schema
+from schema import schema
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^graphql', GraphQLView.as_view(graphiql=True, schema=schema)),
+    url(r'^api/v1', GraphQLView.as_view(graphiql=True, schema=schema)),
 ]
--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/schema.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/schema.py
@@ -1,3 +1,17 @@
 import graphene
 
 from graphene_django.types import DjangoObjectType
+
+# create your schemae and queries here based on your models. For example:
+#
+#from django.contrib.auth.models import User, AbstractBaseUser
+#
+#class UserType(DjangoObjectType):
+#    class Meta:
+#        model = User
+#
+#class Query():
+#    all_users = graphene.List(UserType)
+#
+#    def resolve_all_users(self, info, **kwargs):
+#        return User.objects.all()
